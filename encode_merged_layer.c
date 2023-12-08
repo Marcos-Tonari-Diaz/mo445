127db2dfae86f94794e90595b13bfc01e3a3a260
Marcos Diaz <marcos.dure.diaz@gmail.com>
2023-12-07 22:42:23 -0300

    changed sigmoid implementation

--- encode_merged_layer.c
+++ encode_merged_layer.c
@@ -106,7 +106,21 @@ int main(int argc, char *argv[]) {
           for (int r = 0; r < activ->m; r++){
             // sigmoid
             if (use_sigmoid){
-              activ->val[c][r] = (1 / (1 + pow(EULER_NUMBER, activ->val[c][r])));
+              float v = activ->val[c][r];
+              constexpr float c1 = 0.03138777F;
+              constexpr float c2 = 0.276281267F;
+              constexpr float c_log2f = 1.442695022F;
+              v *= c_log2f*0.5;
+              int intPart = (int)v;
+              float x = (v - intPart);
+              float xx = x * x;
+              float v1 = c_log2f + c2 * xx;
+              float v2 = x + xx * c1 * x;
+              float v3 = (v2 + v1);
+              *((int*)&v3) += intPart << 24;
+              float v4 = v2 - v1;
+              float res = v3 / (v3 - v4); //for tanh change to (v3 + v4)/ (v3 - v4)
+              activ->val[c][r] = res;
             }
             // relu
             if (activ->val[c][r] < 0 && arch->layer[layer-1].relu && !use_sigmoid){
